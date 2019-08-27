fc93ef8cb14fad977401769a18c33aab3e6a1815
= <=>
2019-08-26 22:45:23 -0700

    got led testing working python script side

--- ApartmentFrameworkEmbedded/test/test_scripts/LEDControl_testing/main.py
+++ ApartmentFrameworkEmbedded/test/test_scripts/LEDControl_testing/main.py
@@ -3,15 +3,26 @@ import socket
 
 import led_control
 
-
 strip_one = None
-UDP_IP = "192.168.7.2"
-UDP_PORT = 5005
+UDP_IP = "192.168.86.204"
+UDP_PORT = 5050
 
 sending_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-strip_one = led_control.LEDStrip(1, 144, sending_socket, UDP_IP, UDP_PORT)
+strip_one = led_control.LEDStrip(0, 800, sending_socket, UDP_IP, UDP_PORT)
 
-while(1):
-    # Updating LEDs
-    strip_one.send_dat()
-    time.sleep(1)
+def run_leds():
+    global strip_one
+    for i in range(800):
+        strip_one.set_led(100, 100, 100, i)
+        strip_one.send_dat()
+        time.sleep(.02)
+    for i in range(800):
+        strip_one.set_led(0, 0, 0, i)
+        strip_one.send_dat()
+        time.sleep(.02)
+
+def main():
+    while(1):
+        run_leds()
+
+main()
