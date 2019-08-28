ae4dcb88602bd4bde745b569c3bf09b526d12389
William Redenbaugh <william@matternet.us>
2019-08-27 21:24:00 -0700

    working LED strip control!

--- ApartmentFrameworkEmbedded/test/test_scripts/LEDControl_testing/main.py
+++ ApartmentFrameworkEmbedded/test/test_scripts/LEDControl_testing/main.py
@@ -8,18 +8,21 @@ UDP_IP = "192.168.86.204"
 UDP_PORT = 5050
 
 sending_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-strip_one = led_control.LEDStrip(0, 800, sending_socket, UDP_IP, UDP_PORT)
+strip_one = led_control.LEDStrip(0, 650, sending_socket, UDP_IP, UDP_PORT)
 
 def run_leds():
     global strip_one
-    for i in range(800):
-        strip_one.set_led(100, 100, 100, i)
+
+    for i in range(650):
+        strip_one.set_led(150, 15, 150, i)
         strip_one.send_dat()
-        time.sleep(.02)
-    for i in range(800):
+        time.sleep(.01)
+        
+    for i in range(650):
         strip_one.set_led(0, 0, 0, i)
         strip_one.send_dat()
-        time.sleep(.02)
+        time.sleep(.01)
+
 
 def main():
     while(1):
