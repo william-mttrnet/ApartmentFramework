a042938368135f70cb184a19d9ab9edd6c76a3e0
William Redenbaugh <william@matternet.us>
2019-09-04 20:10:11 -0700

    fixed makefile

--- LEDControlLinux/main.cpp
+++ LEDControlLinux/main.cpp
@@ -21,7 +21,14 @@ uint8_t led_array[NUM_LEDS * 16 + 200];
 
 int main(void){
     strip_one.begin(led_array, NUM_LEDS, 255, 0);
+<<<<<<< HEAD
     //strip_two.begin(led_array, NUM_LEDS, 255, 0);
+=======
+<<<<<<< HEAD
+=======
+    //strip_two.begin(led_array, NUM_LEDS, 255, 0);
+>>>>>>> 730a18e7559b4fcb19dab86876701c9ba7c4dab2
+>>>>>>> c0c95f9b30d33f54f5404420a4cc24c71b12a5b3
     serv_socket.begin(5050);
     
     for(;;){
