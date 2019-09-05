91814437173264d02e8cb63b6d6c6187f71eb323
William Redenbaugh <william@matternet.us>
2019-09-04 23:10:00 -0700

    successfull led matrix udp control complete!

--- LEDMatrixLinux/main.cpp
+++ LEDMatrixLinux/main.cpp
@@ -12,12 +12,14 @@ using rgb_matrix::Canvas;
 // Socket object recieving data
 UDPFrame serv_socket; 
 
+void packet_to_matrix(void);
+
 int main(int argc, char *argv[]){
-    //strip_two.begin(led_array, NUM_LEDS, 255, 0);
-    // serv_socket.begin(5050);
+    serv_socket.begin(5050);
     
     RGBMatrix::Options matrix_options;
     rgb_matrix::RuntimeOptions runtime_opt;
+    
     if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &matrix_options, &runtime_opt)) {
        
     }
@@ -34,19 +36,30 @@ int main(int argc, char *argv[]){
     offscreen->Clear();
     
     offscreen->SetPixel(0, 0, 100, 100, 100);
+    for(uint8_t i = 0; i < 32; i++){
+        for(uint8_t j = 0; j < 64; j++){
+        matrix->SetPixel(j, i, 100, 100, 100);
+        delay(1);
+        }
+    }
     
     for(;;){
-        for(uint8_t i = 0; i < 32; i++){
-            for(uint8_t j = 0; j < 64; j++){
-            matrix->SetPixel(j, i, 100, 100, 100);
-            matrix->SwapOnVSync(offscreen);
-            delay(1);
-                
-            }
-        }
-        
         // Waiting for data
-        // serv_socket.receive();
+        serv_socket.receive();
+        
+        uint8_t m = 0; 
+        for(uint8_t i = 0; i < 4; i++){
+            if(serv_socket.in_array[i] == 15)
+                m++;
+        }
+        if(m == 4){
+            for(uint32_t i = 0; i < 2048; i++){
+                offscreen->SetPixel(i%64, 31 - uint32_t(i/64), serv_socket.in_array[i * 3 + 4], serv_socket.in_array[i * 3 + 5], serv_socket.in_array[i * 3 + 6]);
+            }
+            matrix->SwapOnVSync(offscreen);
+        }            
     }
 }
+
+
 
